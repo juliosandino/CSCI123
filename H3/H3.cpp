931a89de32064a0084ca1f76d6b2bd40db561143
rat395 <rat395@ymail.com>
2016-08-25 18:11:32 -0700

    added homework number 3

---
+++ H3/H3.cpp
@@ -0,0 +1,28 @@
+//H3.cpp
+//Programmer: Julio Sandino                                             CSCI 123, Fall 2016
+//Homework 3                                                            Instructor: Mai, Timothy
+//Due Date: 9/20/16                                                     Date Submitted: 9/20/16
+
+//Program Description: The program prints numbers from 0 to 10, 3 times, while also printing the run number.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+//    for (int i = 1; i <= 3; ++i) {
+//        cout << "Run#" << i << ": ";
+//
+//        for (int j = 1; j <= 10 ; ++j) {
+//            cout << j << " ";
+//        }
+//
+//        cout << endl;
+//    }
+
+    if (5/7){
+        cout << true;
+    }
+
+    return 0;
+}

e68cf22d2cf8e95be0952cedc56a5e84f5ff38b1
rat395 <rat395@ymail.com>
2016-09-15 20:28:18 -0700

    added comments and functionality for multiple lines in input file

--- H6/H6.cpp
+++ H6/H6.cpp
@@ -18,8 +18,11 @@ int main(){
     inputFile.open ("/Users/julio395/ClionProjects/csci123/H6/cad.dat");
     outputFile.open("/Users/julio395/ClionProjects/csci123/H6/outputs.dat");
 
-    if (inputFile.is_open()){
+    //checks to see if the files exist
+    if (inputFile.is_open() && outputFile.is_open()){
+        //loops through every line in the file
         while (getline(inputFile, line)){
+            //loops through every character in the line
             for (int i = 0; i < line.length(); i++) {
 
                 char character = line.at(i);
@@ -40,33 +43,30 @@ int main(){
                 }
             }
 
-            outputLine = line;
-            cout << "Uppercase letters: " << uppercaseLetters << endl;
-            cout << "Lowercase letters: " << lowercaseLetters << endl;
-            cout << line;
+            //adds the modified line to the output file
+            outputFile << line << endl;
         }
+        outputFile.close();
         inputFile.close();
+
+        cout << "Uppercase letters: " << uppercaseLetters << endl;
+        cout << "Lowercase letters: " << lowercaseLetters << endl;
     }
 
     else {
-        cout << "Unable to open input file\n";
-    }
-
-    if (outputFile.is_open()){
-        outputFile << outputLine;
-        outputFile.close();
-    }
-    else{
-        cout << "Unable to open output file\n";
+        cout << "Unable to open input file or output file\n";
     }
 }
 
+//checks if the character is lowercase
 bool isLower(char letter){
     if (letter >= 97 && letter <= 122)
         return true;
     else
         return false;
 }
+
+//checks if the character is uppercase
 bool isUpper(char letter){
     if (letter >= 65 && letter <= 90)
         return true;
@@ -74,6 +74,7 @@ bool isUpper(char letter){
         return false;
 }
 
+//checks if the character is a number
 bool isNumber(char letter){
     if(letter >= 48 && letter <= 57)
         return true;

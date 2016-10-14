ddc5bb1b032875e86da1cfaf17255d7dfbc326f0
rat395 <rat395@ymail.com>
2016-10-13 18:31:58 -0700

    added output file, and also finished the code

--- H8/main.cpp
+++ H8/main.cpp
@@ -8,31 +8,44 @@ int main() {
     string middleName;
     string lastName;
 
-    cout << "What is your first name?";
+    cout << "What is your first name? ";
     cin >> firstName;
 
     char firstNameArr[firstName.length()];
 
     stringToArray(firstName, firstNameArr);
 
-    cout << "What is your Middle name?";
+    cout << "What is your middle name? ";
     cin >> middleName;
 
     char middleNameArr[middleName.length()];
 
     stringToArray(middleName, middleNameArr);
 
-    cout << "What is your first name?";
+    cout << "What is your last name? ";
     cin >> lastName;
 
     char lastNameArr[lastName.length()];
 
     stringToArray(lastName, lastNameArr);
 
-    cout << lastNameArr[0];
+    int fullNameLength = firstName.length() + middleName.length() + lastName.length() + 3;
+
+    char FullName[fullNameLength];
+
+    char space[] = " ";
+
+    strcat(FullName, firstNameArr);
+    strcat(FullName, space);
+    strcat(FullName, middleNameArr);
+    strcat(FullName, space);
+    strcat(FullName, lastNameArr);
+
+    cout << FullName << endl;
+    cout << "Total number of characters: " << fullNameLength - 3;
 }
 
-void stringToArray(string str, char arr[]){
+void stringToArray(string str, char arr[]) {
     for (int i = 0; i < str.length(); i++)
         arr[i] = str.at(i);
 }

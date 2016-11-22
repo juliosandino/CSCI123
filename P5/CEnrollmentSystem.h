dd0f1fac459582a0d078b9097bd8e9d6b7b40098
rat395 <rat395@ymail.com>
2016-11-22 08:40:36 -0800

    Encapsulation of main.cpp in the CEnrollmentSystem Class

---
+++ P5/CEnrollmentSystem.h
@@ -0,0 +1,63 @@
+//
+// Created by Julio Sandino on 11/22/16.
+//
+
+#ifndef P5_CENROLLMENTSYSTEM_H
+#define P5_CENROLLMENTSYSTEM_H
+
+#include "CStudentInfo.h"
+#include "CCourse.h"
+#include "CDepartment.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class CEnrollmentSystem {
+
+public:
+    string courseFilePath;
+    string studentFilePath;
+    string departmentFilePath;
+
+    //constructors
+    CEnrollmentSystem();
+    CEnrollmentSystem(string courses, string students, string departments);
+    ~CEnrollmentSystem();
+
+    //functions
+    void EnrollSystem();
+    int menu ();
+    void listAllDepartments();
+    void addDepartment();
+    bool doesDepartmentExist(string ID);
+    void listAllCourses ();
+    void addCourse();
+    bool doesCourseExist(string CRN);
+    void cancelCourse();
+    void listCoursesOfSpecificDepartment ();
+    void listCoursesWithLessSpecificUnit ();
+    void listCoursesWithEqualSpecificUnit ();
+    void listCoursesWithMoreSpecificUnit ();
+    void listCoursesWithSpecificTitle ();
+    void listCourseInfoWithSpecificCrn ();
+    void listAllStudentsInCourse ();
+    void listAllStudentInfoWithSpecificTerm ();
+    void listAllStudentInfoWithSpecificLastName ();
+    void listStudentInfoWithSpecificId ();
+    void Enroll ();
+    bool isCoursesArrayOpen(CStudentInfo student);
+    void Quit();
+    void readStudentInfo();
+    void writeStudentInfo();
+    void readCourseInfo();
+    void writeCourseInfo();
+    void readDepartmentInfo();
+    int getEnrolledUnits(string studentId);
+    CStudentInfo studentByID(string ID);
+    string searchId(string first, string last);
+    void writeDepartmentInfo();
+};
+
+
+#endif //P5_CENROLLMENTSYSTEM_H
